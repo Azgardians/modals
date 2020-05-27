c414ad3b0e71c6bf96374d76ae706fd4505f2f5e
Azgardians <b517007@iiit-bh.ac.in>
2020-05-28 00:18:34 +0530

    Validation and media Queries added

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ var modalBtn = document.getElementById('modalBtn');
 var submitBtn = document.getElementById('next');
 var closeBtn1 = document.getElementsByClassName('closeBtn')[0];
 var closeBtn2 = document.getElementsByClassName('closeBtn')[1]
+var phoneOption = document.getElementById('phoneOpt');
+var emailOption = document.getElementById('emailOpt');
+var x=0;
 
 modalBtn.addEventListener('click',openModal);
 closeBtn1.addEventListener('click',closeModal1);
 closeBtn2.addEventListener('click',closeModal2);
 submitBtn.addEventListener('click',openSecondModal);
 window.addEventListener('click',clickOutside);
+emailOption.addEventListener('click',emailValidation);
+phoneOption.addEventListener('click',phoneValidation);
 
 
 function openModal () {
@@ -24,10 +29,54 @@ function closeModal1 (){
 function closeModal2 (){
     modal2.style.display = 'none';
 }
+function phoneValidation (){
+    x = 1;
+    // console.log("x = "+x);
+}
+function emailValidation (){
+    x = 2;
+    // console.log("x = "+x);
+}
+function validateEmail(){
+    var emailID = document.myForm.email.value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
 
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+        alert("Please enter correct email ID")
+        document.myForm.email.focus() ;
+        return false;
+    }
+    return( true );
+}
+function validatePhone (){
+    if( document.myForm.phone.value == "" ) {
+        alert( "Please provide your phone!" );
+        document.myForm.phone.focus() ;
+        return false;
+    }
+    return true;
+}
 function openSecondModal (){
-   modal1.style.display = 'none';
-   modal2.style.display = 'block';  
+    var value = false;
+    if(x == 0){
+        alert("Please Check an Option");
+    }
+    if (x == 1){
+        // console.log("phone checked");
+        value = validatePhone();
+    }
+    if (x == 2) {
+        // console.log("email checked");
+        value = validateEmail();
+    }
+    // console.log(value);
+    if(value){
+        modal1.style.display = 'none';
+        modal2.style.display = 'block';
+    }
+    
+     
 }
 
 function clickOutside (e) {
